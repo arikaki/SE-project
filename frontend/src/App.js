c20effae506717df810c3f81e23d7cfdd937f60e
loki <meesalalokesh248@gmail.com>
2022-02-02 20:10:55 -0500

    Sample Testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a sample file.
+          This is a sample testing.
         </p>
         <a
           className="App-link"
