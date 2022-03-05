ace093531a219cc589565d0597c2ea8f2e0d6895
Nikhil1612 <nikhiljiva7@gmail.com>
2022-03-04 21:23:00 -0500

    created categories page and unit test cases

---
+++ frontend/src/Category.js
@@ -0,0 +1,15 @@
+const categories = [
+    "Technology",
+    "Music", 
+    "Photography",
+    "Books",
+    "Sports",
+    "Games",
+    "Religion",
+    "Cars",
+    "Gadgets",
+    "Art",
+    "Health",
+    "History"
+]
+export default categories;
