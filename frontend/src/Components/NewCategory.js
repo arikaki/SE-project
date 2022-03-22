22684336f50215e6d647b886a3d6b0413f52e22b
Nikhil1612 <nikhiljiva7@gmail.com>
2022-03-22 19:41:30 -0400

    added some files

---
+++ frontend/src/Components/NewCategory.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NewCategories.css';
+import Grid from './Grid';
+import CustomButton from "./CustomButton";
+
+function NewCategories() {
+  
+    console.log('New Category rendered')
+  return (
+    <div>
+    <div className='text'> Select atleast five categories</div>
+    <div className="container">
+      <Grid />
+    </div>
+    </div>
+  );
+}
+
+export default NewCategories;
