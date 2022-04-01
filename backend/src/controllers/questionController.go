4d3255fc5c6799939288f2f666ce88149f53e241
Harshwardhan Chauhan <harshwardhan0812@gmail.com>
2022-04-01 18:01:21 -0400

    Added search api for fetching matching questions

--- backend/src/controllers/questionController.go
+++ backend/src/controllers/questionController.go
@@ -9,6 +9,7 @@ func QuestionController(r *mux.Router) {
 	r.HandleFunc("/ask", database.AskQ).Methods("POST")
 	// r.HandleFunc("/edit", database.EditQ).Methods("POST")
 	// r.HandleFunc("/get", database.GetQ).Methods("GET")
+	r.HandleFunc("/search", database.FindMatchingQuestions).Methods("POST")
 	r.HandleFunc("/getAll", database.GetAllQ).Methods("GET")
 	// r.HandleFunc("/getUnanswered", database.GetUnanswered).Methods("GET")
 }
