6a1edf4823e5dd99042ada15fdf433d3b66a2466
loki <meesalalokesh248@gmail.com>
2022-04-01 21:37:57 -0400

    Added test cases for Backend

--- backend/src/controllers/controllers_test.go
+++ backend/src/controllers/controllers_test.go
@@ -51,6 +51,54 @@ func Test_FetchUser(t *testing.T) {
 	}
 }
 
+func Test_DeleteUser(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/deleteuser", nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	a := http.HandlerFunc(database.DeleteUser)
+	resp := httptest.NewRecorder()
+	a.ServeHTTP(resp, req)
+	checkResponseCode(t, http.StatusOK, resp.Code)
+
+	fmt.Println("resp", resp.Body.String())
+
+	if resp.Body.String() != `"User is deleted"` {
+		t.Errorf(`Expected response to be "User is deleted". Got '%v'`, resp.Body.String())
+	}
+}
+func Test_TopQuestion(t *testing.T) {
+	var jsonStr = []byte(`{"topic": "Technology"}`)
+	req, _ := http.NewRequest("POST", "/topquestion", bytes.NewReader(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	a := http.HandlerFunc(database.DeleteUser)
+	resp := httptest.NewRecorder()
+	a.ServeHTTP(resp, req)
+	checkResponseCode(t, http.StatusOK, resp.Code)
+
+	fmt.Println("resp", resp.Body.String())
+
+	if resp.Body.String() != `{"question":"How can modern technology help evolve?","question":"What does “absolute refractive index of glass is 1.5” mean?","question":"what is the speed of the bullet train?"}` {
+		t.Errorf(`Expected product name to be "Login Successful". Got '%v'`, resp.Body.String())
+	}
+}
+func Test_SelectedQuestion(t *testing.T) {
+	var jsonStr = []byte(`{"_id": "62217301ecf350ef0c2e0dc5"}`)
+	req, _ := http.NewRequest("POST", "/selectedquestion", bytes.NewReader(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	a := http.HandlerFunc(database.DeleteUser)
+	resp := httptest.NewRecorder()
+	a.ServeHTTP(resp, req)
+	checkResponseCode(t, http.StatusOK, resp.Code)
+
+	fmt.Println("resp", resp.Body.String())
+
+	if resp.Body.String() != `{"answer":"Technology has helped us to fly, drive, sail,communicate"}` {
+		t.Errorf(`Expected product name to be "Feteched Sucessfully". Got '%v'`, resp.Body.String())
+	}
+}
+
 func Test_Logout(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/logout", nil)
 	resp := httptest.NewRecorder()
