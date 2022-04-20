b7dce6b2a52e8d663f464dae3a047e2500184b3d
SaiRishab <78468475+arikaki@users.noreply.github.com>
2022-04-20 15:02:06 -0400

    logo on main screen

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -3,6 +3,7 @@ import "./Login.css";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../firebase";
 import axios from "axios";
+import logo1 from "../images/kora1.png";
 
 function Login(props) {
   const handleSubmit = async () => {
@@ -44,6 +45,15 @@ function Login(props) {
   return (
     <div className="login-container">
       <div className="login-content">
+      <img
+          src={logo1}
+          alt="logo"
+          style={{
+            borderRadius: "20px",
+            width: "500px",
+            marginBottom: "-20px",
+          }}
+        />
         <button onClick={handleSubmit} className="btn-login">
           Login to continue
         </button>
